db01f39471f44506c69fd34a3cd6a22d40c79fa6
miakotone <mia.kotone@gmail.com>
2020-08-16 12:40:53 +0900

    initial commit

---
+++ pick_a_card.rb
@@ -0,0 +1,5 @@
+
+value = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] #Bubi, Chervi, Kresti, Piki
+
+puts "#{value.sample} of #{suits.sample}"
